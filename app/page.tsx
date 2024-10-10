19259bb21d19bc10bac6c7713f38bd933cea1839
Bisma <63401738+bisma777@users.noreply.github.com>
2024-10-10 19:12:29 +0530

    fonts updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import Banner from "@/components/home/Banner";
 import Footer from "@/components/home/Footer";
 import Header from "@/components/layout/Header";
+import {Poppins} from  'next/font/google'
 
+const poppins = Poppins({
+  subsets: ['latin'], 
+  weight: ['400', '500', '600', '700'],
+  display: 'swap',
+});
 export default function Home() {
   return (
-    <div>
+    <div  className={poppins.className}>
       <div className=" bg-gradient-to-br from-[#08b0be] to-[#087ad5]">
         <Header />
         <Banner/>
